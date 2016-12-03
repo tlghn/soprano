0a22a93f8ac7db261f4c3e43516151d48b36bf95
Tolgahan Albayrak <tolgahan.albayrak@live.com>
2016-12-03 11:55:46 +0200

    Release 1.0.4

--- core/debug.js
+++ core/debug.js
@@ -2,7 +2,10 @@
  * Created by tolgahan on 17.11.2016.
  */
 const path = require('path');
-const root = path.resolve(__dirname + '/../');
+const root = path.normalize(path.dirname(require.main.filename));
+const SEP = path.normalize('/');
+const NODE_MODULES = `${SEP}node_modules${SEP}`;
+
 function _getCallerFile() {
     var originalFunc = Error.prepareStackTrace;
 
@@ -22,11 +25,23 @@ function _getCallerFile() {
         }
     } catch (e) {}
 
+
+
     Error.prepareStackTrace = originalFunc;
 
-    return path.basename(callerfile.substr(root.length + 1).replace(/\\+|\/+/g, ':'), '.js');
+    currentfile = path.normalize(currentfile);
+
+    let modulesIndex = callerfile.indexOf(NODE_MODULES);
+    let appPath = root;
+    if(modulesIndex > -1){
+        appPath = callerfile.substr(0, callerfile.indexOf(SEP, modulesIndex + NODE_MODULES.length + 1));
+    }
+
+    let appName = path.basename(appPath);
+
+    return `${appName}:${path.basename(callerfile.substr(appPath.length + 1).replace(/\\+|\/+/g, ':'), '.js')}`;
 }
 
 module.exports = function () {
-    return require('debug')('soprano:' + _getCallerFile());
+    return require('debug')(_getCallerFile());
 };
